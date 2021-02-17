0d6377d9d0d70d11cd37c74b230ec2be1321920d
Igor Ferreira <65921873+ferreira-igor@users.noreply.github.com>
2021-02-17 11:54:25 -0300

    Update StationSetup.sh

--- StationSetup.sh
+++ StationSetup.sh
@@ -7,13 +7,40 @@ echo "Removendo eventuais travas do apt..."
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
-echo "Atualizando o repositório..."
+echo "Adicionando repositório de terceiros..."
+
+curl -sS https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+curl -sS https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb [arch=amd64] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+echo "Atualizando os repositórios..."
 
 sudo apt update
 
 echo "Instalando aplicativos..."
 
-sudo apt install lsb tlp tlp-rdw build-essential git-all python3-all idle thonny esptool mint-meta-codecs exfat-fuse exfat-utils fonts-noto fonts-roboto gimp inkscape kicad audacity -y
+# Aplicativos de terceiros
+sudo apt install google-chrome-stable code spotify-client -y
+
+# Aplicativos para desenvolvimento
+sudo apt install build-essential git-all python3-all idle thonny esptool kicad -y
+
+# Utilitários
+sudo apt install lsb tlp tlp-rdw mint-meta-codecs exfat-fuse exfat-utils -y
+
+# Personalização
+sudo apt install fonts-noto fonts-roboto papirus-icon-theme -y
+
+# Games
+sudo apt install retroarch -y
+
+# Outros
+sudo apt install gimp inkscape audacity -y
 
 echo "Instalando bloqueador de anúncios..."
 
