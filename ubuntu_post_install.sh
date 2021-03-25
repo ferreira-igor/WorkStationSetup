9fa0f02c0b35f206b33ad35f5f49300173ff061e
Igor Ferreira <65921873+ferreira-igor@users.noreply.github.com>
2021-03-25 19:47:21 -0300

    Update ubuntu_post_install.sh

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-### SCRIPT DE PÓS INSTALAÇÃO PARA O UBUNTU 20.04 ###
+### SCRIPT DE PÓS INSTALAÇÃO PARA O UBUNTU 20.04
 
-## BYE BYE SNAPS ##
+## BYE BYE SNAPS
 
-for p in $(snap list | awk '{print $1}'); do
-  sudo snap remove $p
+for snap_name in $(snap list | awk '{print $1}'); do
+    sudo snap remove "$snap_name"
 done
 
 sudo systemctl stop snapd
 
 for m in /snap/core/*; do
-   sudo umount $m
+    sudo umount "$m"
 done
 
 sudo umount /var/snap
@@ -19,7 +19,7 @@ sudo umount /var/snap
 sudo snap remove core
 sudo snap remove core18
 
-sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
+sudo apt-get purge snapd -y
 
 rm -rf ~/snap
 sudo rm -rf /snap
@@ -27,13 +27,13 @@ sudo rm -rf /var/snap
 sudo rm -rf /var/lib/snapd
 sudo rm -rf /var/cache/snapd
 
-sudo cat << EOF > /etc/apt/preferences.d/snapd
+sudo tee /etc/apt/preferences.d/snapd << EOF
 Package: snapd
 Pin: origin *
 Pin-Priority: -1
 EOF
 
-## ADIÇÃO DE REPOSITÓRIOS DE TERCEIROS ##
+## ADIÇÃO DE REPOSITÓRIOS DE TERCEIROS
 
 wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
@@ -47,19 +47,21 @@ echo "deb [arch=amd64] http://repository.spotify.com stable non-free" | sudo tee
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
 echo "deb [arch=amd64] http://apt.insync.io/ubuntu focal non-free contrib" | sudo tee /etc/apt/sources.list.d/insync.list
 
-## REMOÇÃO DE EVENTUAIS TRAVAS DO APT ##
+## REMOÇÃO DE EVENTUAIS TRAVAS DO APT
 
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
-## ATUALIZAÇÃO DOS REPOSITÓRIOS ##
+## ATUALIZAÇÃO DOS REPOSITÓRIOS
 
-sudo apt update
+sudo apt-get update
 
-## INSTALAÇÃO DOS APLICATIVOS ##
+## INSTALAÇÃO DOS APLICATIVOS
 
-apps_list = (
+apps_list=(
+    # Gerenciadores de pacotes
     synaptic
+    # Desenvolvimento
     build-essential
     git-all
     python3-all
@@ -69,6 +71,7 @@ apps_list = (
     esptool
     cutecom
     putty
+    # Ferramentas do sistema
     p7zip-full
     unrar
     curl
@@ -76,37 +79,58 @@ apps_list = (
     tlp
     exfat-fuse
     zram-config
-    gnome-boxes
-    gnome-tweaks
     timeshift
+    gnome-boxes
+    remmina
+    deja-dup
+    vino
+    baobab
+    usb-creator-gtk
+    branding-ubuntu
+    transmission-gtk
+    # Multimídia
     ubuntu-restricted-addons
     totem
     rhythmbox
     vlc
     qwinff
+    # Personalização
+    gnome-tweaks
     fonts-noto
     fonts-roboto
+    # Engenharia
     kicad
     freecad
     librecad
+    # Fotografia
     gimp
     inkscape
+    shotwell
+    cheese
+    # Escritório
+    libreoffice
+    simple-scan
+    gnome-calendar
+    gnome-todo
+    thunderbird
+    # Aplicativos de terceiros
     google-chrome-stable
     code
     spotify-client
     insync
     insync-nautilus
-    )
+)
 
-for app_name in ${apps_list[@]}; do
-    if ! dpkg -l | grep -q $app_name; then
-        sudo apt install "$app_name" -y
+for app_name in "${apps_list[@]}"; do
+    if ! dpkg -l | awk '{print $2}' | grep -qw "$app_name"; then
+        echo "[INSTALANDO] - $app_name"
+        sudo apt-get install "$app_name" -y
     else
         echo "[INSTALADO] - $app_name"
     fi
 done
 
-## INSTALAÇÃO DO BLOQUEADOR DE ANÚNCIOS ##
+## INSTALAÇÃO DO BLOQUEADOR DE ANÚNCIOS
 
 curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.2.0/hblock'
 echo 'b9ed6de52455fbde882879ef50470c1538bc5ac8d1479ef130442770b159dbe3  /tmp/hblock' | shasum -c
@@ -125,13 +149,15 @@ sudo systemctl daemon-reload
 sudo systemctl enable hblock.timer
 sudo systemctl start hblock.timer
 
-## ADIÇÃO DO USUÁRIO AO GRUPO DIALOUT ##
+## ADIÇÃO DO USUÁRIO AO GRUPO DIALOUT
 
-sudo usermod -a -G dialout $USER
+sudo usermod -aG dialout $USER
 
-## ATUALIZAÇÃO DO SISTEMA ##
+## ATUALIZAÇÃO DO SISTEMA
 
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean -y
+sudo apt-get check -y
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+
+## FIM
